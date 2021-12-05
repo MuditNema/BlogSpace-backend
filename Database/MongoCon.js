112e2d125783083318c1ec0a807432d30e937160
Mudit Nema <muditnema14@gmail.com>
2021-12-05 16:07:49 +0530

    connecting DB to cluster instead of localhost

--- Database/MongoCon.js
+++ Database/MongoCon.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'); 
-const mongoURI = 'mongodb://localhost:27017/blogspace?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
-
+// const mongoURI = 'mongodb://localhost:27017/blogspace?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false'
+const mongoURI = 'mongodb+srv://atsumu_miya:Atsumu_miya123@cluster0.fda3r.mongodb.net/blogspace?authSource=admin&replicaSet=atlas-wgpzno-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
 
 const connectToMongo = async () =>{
     mongoose.connect(mongoURI,()=>{
